8aa2e908cced2b3ebc40babd197647f980b51e9c
Jananta Dhafine <dhafinekresna@gmail.com>
2025-11-17 01:48:07 +0700

    Update TaskTable.php

--- app/Models/TaskTable.php
+++ app/Models/TaskTable.php
@@ -8,8 +8,25 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class TaskTable extends Model
 {
     protected $guarded = [];
+    protected $fillable = [
+        'judul',
+        'deskripsi',
+        'deadline',
+        'list_id',  // Sesuaikan dengan nama kolom di database
+        'sudah_selesai',
+        'user_id'
+    ];
+    protected $casts = [
+        'sudah_selesai' => 'boolean',
+        'deadline' => 'date',
+    ];
 
     public function List(): BelongsTo{
-        return $this->belongsTo(ListTable::class, 'list_tables_id');
+        return $this->belongsTo(ListTable::class, 'list_id','id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
